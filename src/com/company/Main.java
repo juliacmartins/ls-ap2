64bb16930d1a26ae51998bf7d9adfd382ae485ea
Julia Martins <juliaacmartins@gmail.com>
2021-10-31 22:05:09 -0300

    Versão Final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,61 @@
 package com.company;
 
+import java.text.ParseException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+         Teclado.nome("Meu nome é: Maria");
+         Teclado.lerNome();
 
-    Teclado.nome("Meu nome é: Maria");
-    Teclado.lerNome();
-    Teclado.peso("Meu peso é 55kg");
-    Teclado.lerPeso();
-    Teclado.sexo("Sou do Sexo Femino");
-    Teclado.lerSexo();
-    Teclado.idade("Tenho 15 anos");
-    Teclado.lerIdade();
-    Teclado.nota("Tirei 8 em Matemática");
-    Teclado.lerNota();
-    Teclado.validarEmail("juliaacmartinsgmail.com");
-    Teclado.validarEmail("juliaacmartins@gmail.com");
-    Teclado.validarTelefone("1l235");
-    Teclado.validarTelefone("51-991682204");
-    Teclado.validarData("10/01/2015");
-    Teclado.validarData("2015/1/2");
-    Teclado.validarNumero("20123");
-    Teclado.validarNumero("20l23");
+         Teclado.peso("Meu peso é 55kg");
+         Teclado.lerPeso();
+
+         Teclado.sexo("Sou do Sexo Femino");
+         Teclado.lerSexo();
+
+         Teclado.idade("Tenho 15 anos");
+         Teclado.lerIdade();
+
+         Teclado.nota("Tirei 8 em Matemática");
+         Teclado.lerNota();
+
+        Teclado.lerEmail();
+        String email = Teclado.validarEmail();
+
+        if (email != null) {
+            System.out.println("Email " + email + " é válido");
+        } else {
+            System.out.println("Email informado é inválido");
+        }
+
+        Teclado.lerTelefone();
+        String telefone = Teclado.validarTelefone();
+
+        if (telefone != null) {
+            System.out.println("Telefone " + telefone + " é válido");
+        } else {
+            System.out.println("Telefone informado é inválido");
+        }
+
+        Teclado.lerData();
+        String data = Teclado.validarData();
+
+        if (data != null) {
+            String formatedData = Teclado.converterData(data, "dd/MM/yyyy", "MM/dd/yyyyy");
+            System.out.println("Data digitada: " + data);
+            System.out.println("Data invertida: " + formatedData);
+        } else {
+            System.out.println("Data não pode ser convertida porque é inválida");
+        }
+
+        Teclado.lerNumero();
+        CharSequence numero = Teclado.validarNumero();
+
+        if (numero != null) {
+            System.out.println("Numero " + numero + " é válido");
+        } else {
+            System.out.println("Numero informado é inválido");
+        }
     }
 }
--- src/com/company/Teclado.java
+++ src/com/company/Teclado.java
@@ -1,119 +1,176 @@
 package com.company;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 
-public class Teclado{
+public class Teclado {
     public static String nome;
     public static double peso;
     public static Character sexo;
     public static int idade;
     public static float nota;
+    public static String data;
+    public static String email;
+    public static String telefone;
+    public static CharSequence numero;
 
 
     public static void nome(String message){
         System.out.println(message);
     }
+
     public static void lerNome() {
         Scanner tc = new Scanner(System.in);
         System.out.println("Qual seu nome?");
-        nome=tc.next();
+        nome = tc.next();
     }
+
     public static void peso(String message){
         System.out.println(message);
     }
+
     public static void lerPeso() {
         Scanner tc = new Scanner(System.in);
         System.out.println("Qual seu peso?");
-        peso= tc.nextDouble();
+        peso = tc.nextDouble();
     }
+
     public static void sexo(String message){
         System.out.println(message);
     }
+
     public static void lerSexo() {
         Scanner tc = new Scanner(System.in);
         System.out.println("E você?");
         sexo= tc.next().charAt(0);
     }
+
     public static void idade(String message){
         System.out.println(message);
     }
+
     public static void lerIdade() {
         Scanner tc = new Scanner(System.in);
         System.out.println("Qual sua Idade?");
         idade= tc.nextInt();
     }
+
     public static void nota(String message){
         System.out.println(message);
     }
+
     public static void lerNota() {
         Scanner tc = new Scanner(System.in);
         System.out.println("Qual nota você tirou?");
-        nota= tc.nextFloat();
-   }
-    public static boolean compararEmail(String email) {
+        nota = tc.nextFloat();
+    }
+
+    public static void lerEmail() {
+        Scanner tc = new Scanner(System.in);
+        System.out.println("Qual seu e-mail?");
+        email = tc.next();
+    }
+
+    private static boolean compararEmail(String email) {
         String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
                 "[a-zA-Z0-9_+&*-]+)*@" +
                 "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
                 "A-Z]{2,7}$";
-
         Pattern pat = Pattern.compile(emailRegex);
+
         if (email == null)
             return false;
+
         return pat.matcher(email).matches();
     }
-    public static void validarEmail(String email) {
-        System.out.println("Teste Email");
-        if (compararEmail(email))
-            System.out.println("Você digitou: " +email +" e ele é válido.");
-        else
-            System.out.println("Você digitou: " +email +" e ele não é válido.");
-    }
-    public static boolean compararTelefone(CharSequence telefone) {
-        String telefoneRegex = "[0-9]{2}-([0-9]{8}|[0-9]{9})";
 
+    public static String validarEmail() {
+        if (compararEmail(email))
+            return email;
+        else
+            return null;
+    }
+
+    public static void lerTelefone(){
+        Scanner tc = new Scanner(System.in);
+        System.out.println("Digite seu Telefone:");
+        telefone = tc.next();
+    }
+
+    private static boolean compararTelefone(String telefone) {
+        String telefoneRegex = "^[0][0-9]{2}-([0-9]{8}|[0-9]{9})";
         Pattern pat = Pattern.compile(telefoneRegex);
+
         if (telefone == null)
             return false;
+
         return pat.matcher(telefone).matches();
     }
-    public static void validarTelefone(CharSequence telefone) {
-        System.out.println("Teste Telefone");
-        if (compararTelefone(telefone))
-            System.out.println("Você digitou: " +telefone+" e ele é válido.");
-        else
-            System.out.println("Você digitou: " +telefone +" e ele não é válido.");
-    }
-    public static boolean compararData(CharSequence data) {
-        String dataRegex = "\\d{2}/\\d{2}/\\d{4}";
 
+    public static String validarTelefone() {
+        if (compararTelefone(telefone))
+            return telefone;
+        else
+            return null;
+    }
+
+      public static void lerData() {
+        Scanner tc = new Scanner(System.in);
+        System.out.println("Digite uma data:");
+        data = tc.next();
+    }
+
+    public static String validarData() {
+        if (compararData(data))
+            return data;
+        else
+            return null;
+    }
+
+    private static boolean compararData(CharSequence data) {
+        String dataRegex = "\\d{2}/\\d{2}/\\d{4}";
         Pattern pat = Pattern.compile(dataRegex);
+
         if (data == null)
             return false;
+
         return pat.matcher(data).matches();
     }
-    public static void validarData(CharSequence data) {
-        System.out.println("Teste Data:");
-        if (compararData(data))
-            System.out.println("Você digitou: " +data+" e ele é válido.");
-        else
-            System.out.println("Você digitou: " +data +" e ele não é válido.");
-    }
-    public static boolean compararNumero(CharSequence numero) {
-        String numeroRegex = "^[0-9]+$";
 
+    public static String converterData(String date, String dateFormat, String dateFormatOut) throws ParseException {
+        SimpleDateFormat formato = new SimpleDateFormat(dateFormat);
+        Date newDate = formato.parse(date);
+
+        formato.applyPattern(dateFormatOut);
+
+        return formato.format(newDate);
+    }
+
+    public static void lerNumero() {
+        Scanner tc = new Scanner(System.in);
+        System.out.println("Digite um numero: ");
+        numero = tc.next();
+    }
+
+    private static boolean compararNumero(CharSequence numero) {
+        String numeroRegex = "^[0-9]+$";
         Pattern pat = Pattern.compile(numeroRegex);
+
         if (numero == null)
             return false;
+
         return pat.matcher(numero).matches();
     }
-    public static void validarNumero(CharSequence numero) {
-        System.out.println("Teste Numero");
+
+    public static CharSequence validarNumero() {
         if (compararNumero(numero))
-            System.out.println("Você digitou: " +numero+ " e ele é válido.");
+            return numero;
         else
-            System.out.println("Você digitou: " +numero+ " e ele não é válido.");
+            return null;
     }
 }
 
